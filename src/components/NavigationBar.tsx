2a879a5db792f65fb0a984d4527549df6cbe9d54
Yash Tibrewal <yashtibrewal@Yashs-MacBook-Air.local>
2022-04-05 22:25:49 +0530

    v2

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -6,10 +6,10 @@ function NavigationBar() {
             <a className="list-item" href="#aboutMe">About Me</a>
             <a className="list-item" href="#certificates">Certificates</a>
             <a className="list-item" href="#professionalCertificates">Work</a>
-            <a className="list-item" href="#aptitudeTests">Apitutude Tests</a>
+            <a className="list-item" href="#aptitudeTests">Apitutude</a>
             <a className="list-item" href="#education">Education</a>
-            <a className="list-item" href="#googleCertificates">Google Certificates</a>
-            <a className="list-item" href="#workshopCertificates">Workshop Certificates</a>
+            <a className="list-item" href="#googleCertificates">Google</a>
+            <a className="list-item" href="#workshopCertificates">Workshop</a>
         </div>
 
     );

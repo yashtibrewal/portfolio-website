f665a54b3f39203ab0a4b0b2ef52c456a3f11b75
Yash Tibrewal <yashtibrewal@Yashs-MacBook-Air.local>
2022-04-05 17:15:15 +0530

    checkpoint

--- src/components/ContactMe.tsx
+++ src/components/ContactMe.tsx
@@ -3,16 +3,33 @@ import EmailIcon from '@mui/icons-material/Email';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
+import Grid from '@mui/material/Grid';
+
 function ContactMe() {
     return (
         <div id="contactMe">
-            <PermPhoneMsgIcon className='pm'></PermPhoneMsgIcon><div className='pm'> +91 88790 34882</div>
-            <EmailIcon className='pm'></EmailIcon><div className='pm'>yashtibrewal@ufl.edu</div>
-            <EmailIcon className='pm'></EmailIcon><div className='pm'>yashkush.tibrewal@gmail.com</div>
-            <InstagramIcon className='pm'></InstagramIcon><div className='pm'><a className='pb' href='https://www.instagram.com/yash.tibrewal.17/'>Instagram</a></div>
-            <LinkedInIcon className='pm'></LinkedInIcon><div className='pm'><a className='pb' href='https://www.linkedin.com/in/yash-tibrewal/'>LinkedIn</a></div>
-            <GitHubIcon className='pm'></GitHubIcon><div className='pm'><a className='pb' href='https://github.com/yashtibrewal'>Github</a></div>
+            <Grid container xs={12} >
+                <Grid item >
+                    <PermPhoneMsgIcon className='pm'></PermPhoneMsgIcon><span className='pm'> +91 88790 34882</span>
+                </Grid>
+                <Grid item xs={12} sm={6} md={2} lg={1}>
+                    <EmailIcon className='pm'></EmailIcon><span className='pm'>yashtibrewal@ufl.edu</span>
+                </Grid>
+                <Grid item xs={12} sm={6} md={2} lg={1}>
+                    <InstagramIcon className='pm'></InstagramIcon><span className='pm'><a className='pb' href='https://www.instagram.com/yash.tibrewal.17/'>Instagram</a></span>
+                </Grid>
+                <Grid item xs={12} sm={6} md={2} lg={1}>
+                    <EmailIcon className='pm'></EmailIcon><span className='pm'>yashkush.tibrewal@gmail.com</span>
+                </Grid>
+                <Grid item xs={12} sm={6} md={2} lg={1}>
+                    <LinkedInIcon className='pm'></LinkedInIcon><span className='pm'><a className='pb' href='https://www.linkedin.com/in/yash-tibrewal/'>LinkedIn</a></span>
+                </Grid>
+                <Grid item xs={12} sm={6} md={2} lg={1}>
+                    <GitHubIcon className='pm'></GitHubIcon><span className='pm'><a className='pb' href='https://github.com/yashtibrewal'>Github</a></span>
+                </Grid>
+            </Grid>
         </div>
+
     )
 }
 

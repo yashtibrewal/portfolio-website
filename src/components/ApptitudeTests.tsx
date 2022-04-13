46e6fab4383de1ce5425963707f261287c37437f
Yash Tibrewal <yashtibrewal@Yashs-MacBook-Air.local>
2022-04-13 08:35:43 +0530

    closes #10

--- src/components/ApptitudeTests.tsx
+++ src/components/ApptitudeTests.tsx
@@ -1,8 +1,6 @@
 
 import { Typography } from '@mui/material';
 import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
 import Grid from '@mui/material/Grid';
 
 function AptitudeTests() {
@@ -11,69 +9,47 @@ function AptitudeTests() {
             <h1 className='logo'>Aptitude Tests</h1>
             <Grid container spacing={2}>
                 <Grid xs={6} sm={4} md={3} lg={2} item>
-                    <Card>
-                        <CardMedia
-                            component="img"
-                            image="gre.webp"
-                            alt="Logo could not be loaded"
-                        />
+                    <Card className='pm'>
+                        <Typography>
+                            GRE: 310/340
+                        </Typography>
                     </Card>
                 </Grid>
-                <Grid  xs={6} sm={4} md={3} lg={2} item >
-                    <Card>
-                        <CardMedia
-                            component="img"
-                            image="/apptitude/CAT-2018-1000x600.webp"
-                            alt="Logo could not be loaded"
-                        />
+                <Grid xs={6} sm={4} md={3} lg={1} item >
+                    <Card  className='pm'>
+                        <Typography>
+                            CAT
+                        </Typography>
                     </Card>
                 </Grid>
-                <Grid  xs={6} sm={4} md={3} lg={2} item>
-                    <Card>
-                        <CardMedia
-                            component="img"
-                            image="/apptitude/TOEFL.webp"
-                            alt="Logo could not be loaded"
-                        />
+                <Grid xs={6} sm={4} md={3} lg={2} item>
+                    <Card  className='pm'>
+                        <Typography>
+                            TOEFL: 95/120
+                        </Typography>
+                    </Card>
+                </Grid>
+                <Grid xs={6} sm={4} md={3} lg={1} item >
+                    <Card  className='pm'>
+                        <Typography>SNAP</Typography>
+                    </Card>
+                </Grid>
+                <Grid xs={6} sm={4} md={3} lg={1} item >
+                    <Card  className='pm'>
+                        <Typography>XAT</Typography>
                     </Card>
                 </Grid>
                 <Grid xs={6} sm={4} md={3} lg={2} item >
-                    <Card>
-                        <CardMedia
-                            component="img"
-                            image="/apptitude/snap.webp"
-                            alt="Logo could not be loaded"
-                        />
+                    <Card  className='pm'>
+                        <Typography>SDA Bocconi: 100%</Typography>
                     </Card>
                 </Grid>
-                <Grid  xs={6} sm={4} md={3} lg={2} item >
-                    <Card>
-                        <CardMedia
-                            component="img"
-                            image="/apptitude/XAT.webp"
-                            alt="Logo could not be loaded"
-                        />
+                <Grid xs={6} sm={4} md={3} lg={1} item >
+                    <Card  className='pm'>
+                        <Typography>NMAT</Typography>
                     </Card>
                 </Grid>
-                <Grid  xs={6} sm={4} md={3} lg={2} item >
-                    <Card >
-                        <CardMedia
-                            component="img"
-                            image="/apptitude/SDA.webp"
-                            alt="Logo could not be loaded"
-                        />
-                    </Card>
-                </Grid>
-                <Grid  xs={6} sm={4} md={3} lg={2} item >
-                    <Card>
-                        <CardMedia
-                            component="img"
-                            image="/apptitude/MukeshPatelSchoolLogo.webp"
-                            alt="Logo could not be loaded"
-                        />
-                    </Card>
-                </Grid>
-                
+
             </Grid>
         </div>
     );

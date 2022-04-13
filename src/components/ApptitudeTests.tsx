ec0ad328973cf2aff85f150760ca4328c2c12492
Yash Tibrewal <yashtibrewal@Yashs-MacBook-Air.local>
2022-04-13 07:45:07 +0530

    closes #15

--- src/components/ApptitudeTests.tsx
+++ src/components/ApptitudeTests.tsx
@@ -1,4 +1,5 @@
 
+import { Typography } from '@mui/material';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
@@ -8,103 +9,71 @@ function AptitudeTests() {
     return (
         <div id="aptitudeTests">
             <h1 className='logo'>Aptitude Tests</h1>
-            <Grid container>
-                <Grid xs={12} sm={6} md={4} className='aptitutdeTestItems' item >
+            <Grid container spacing={2}>
+                <Grid xs={6} sm={4} md={3} lg={2} item>
                     <Card>
                         <CardMedia
                             component="img"
-                            sx={{ width: 300 }}
                             image="gre.webp"
                             alt="Logo could not be loaded"
                         />
-                        <CardContent className="fs2rem">
-                            310
-                        </CardContent>
-
                     </Card>
                 </Grid>
-                <Grid xs={12} sm={6} md={4} className='aptitutdeTestItems' item >
-                    <Card>
-                        <CardMedia
-                            className='aptitudeItem'
-                            component="img"
-                            image="/apptitude/TOEFL.webp"
-                            sx={{ width: 300 }}
-                            alt="Logo could not be loaded"
-                        />
-                        <CardContent className='fs2rem'>
-                            95
-                        </CardContent>
-
-                    </Card>
-                </Grid>
-                <Grid xs={12} sm={6} md={4} className='aptitutdeTestItems' item >
+                <Grid  xs={6} sm={4} md={3} lg={2} item >
                     <Card>
                         <CardMedia
                             component="img"
-                            sx={{ width: 300 }}
                             image="/apptitude/CAT-2018-1000x600.webp"
                             alt="Logo could not be loaded"
                         />
-                        <CardContent className="fs2rem">
-                            Percentile 74.14
-                        </CardContent>
-
                     </Card>
                 </Grid>
-                <Grid xs={12} sm={6} md={4} className='aptitutdeTestItems' item >
+                <Grid  xs={6} sm={4} md={3} lg={2} item>
                     <Card>
                         <CardMedia
                             component="img"
-                            sx={{ width: 300 }}
-                            image="/apptitude/XAT.webp"
+                            image="/apptitude/TOEFL.webp"
                             alt="Logo could not be loaded"
                         />
-                        <CardContent className="fs2rem">
-                            Percentile 59.102
-                        </CardContent>
-
                     </Card>
                 </Grid>
-                <Grid xs={12} sm={6} md={4} className='aptitutdeTestItems' item >
-                    <Card >
-                        <CardMedia
-                            component="img"
-                            sx={{ width: 300 }}
-                            image="/apptitude/SDA.webp"
-                            alt="Logo could not be loaded"
-                        />
-                        <CardContent className="fs2rem">
-                            40% Scholarship
-                        </CardContent>
-                    </Card>
-                </Grid>
-                <Grid xs={12} sm={6} md={4} className='aptitutdeTestItems' item >
+                <Grid xs={6} sm={4} md={3} lg={2} item >
                     <Card>
                         <CardMedia
                             component="img"
-                            sx={{ width: 300 }}
-                            image="/apptitude/MukeshPatelSchoolLogo.webp"
-                            alt="Logo could not be loaded"
-                        />
-                        <CardContent className="fs2rem">
-                            Scaled Score 189
-                        </CardContent>
-                    </Card>
-                </Grid>
-                <Grid xs={12} sm={6} md={4} className='aptitutdeTestItems' item >
-                    <Card>
-                        <CardMedia
-                            component="img"
-                            sx={{ width: 300 }}
                             image="/apptitude/snap.webp"
                             alt="Logo could not be loaded"
                         />
-                        <CardContent className="fs2rem">
-                            Percentile 54.1548
-                        </CardContent>
                     </Card>
                 </Grid>
+                <Grid  xs={6} sm={4} md={3} lg={2} item >
+                    <Card>
+                        <CardMedia
+                            component="img"
+                            image="/apptitude/XAT.webp"
+                            alt="Logo could not be loaded"
+                        />
+                    </Card>
+                </Grid>
+                <Grid  xs={6} sm={4} md={3} lg={2} item >
+                    <Card >
+                        <CardMedia
+                            component="img"
+                            image="/apptitude/SDA.webp"
+                            alt="Logo could not be loaded"
+                        />
+                    </Card>
+                </Grid>
+                <Grid  xs={6} sm={4} md={3} lg={2} item >
+                    <Card>
+                        <CardMedia
+                            component="img"
+                            image="/apptitude/MukeshPatelSchoolLogo.webp"
+                            alt="Logo could not be loaded"
+                        />
+                    </Card>
+                </Grid>
+                
             </Grid>
         </div>
     );

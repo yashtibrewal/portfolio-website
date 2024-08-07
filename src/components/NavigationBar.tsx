e388fff0c5caedc607f41b65f38af61292f10bd0
Yash Tibrewal <yashkush.tibrewal@gmail.com>
2024-08-07 19:41:56 +0530

    added transition effect removed extra css

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,12 +1,19 @@
-import { EventHandlers } from "@mui/material";
-import { EventHandler, SyntheticEvent, useState } from "react";
+// NavigationBar.tsx
+import { useState } from "react";
+import '../css/Panel.css';
 
 function NavigationBar() {
 
-    const [onClicked, setOnClicked] = useState(false)
+    const [isPanelOpen, setIsPanelOpen] = useState(false)
+
+    const togglePanel = () => {
+        console.log(isPanelOpen);
+        setIsPanelOpen(!isPanelOpen);
+    }
 
     return (
         <div>
+
             <div className="bg-my-blue fixed z-50 right-0 top-0 w-100 pt-5 pr-5 hidden lg:block">
                 <a className="m-2 ml-7 text-my-voilet text-2xl font-semibold font-mono" href="#aboutMe">About Me</a>
                 <a className="m-2 ml-7 text-my-voilet text-2xl font-semibold font-mono" href="#certificates">Certificates</a>
@@ -17,12 +24,8 @@ function NavigationBar() {
                 <a className="m-2 ml-7 text-my-voilet text-2xl font-semibold font-mono" href="#workshopCertificates">Workshop</a>
             </div>
             <div
-                id="test"
-                onClick={() => {
-                    setOnClicked(!onClicked)
-                }}
+                onClick={togglePanel}
                 className="fixed top-5 z-40 right-5 bg-my-yellow rounded-xl w-11 h-11 visbile lg:hidden">
-
                 <div>
                     <div className="bg-my-blue h-1 m-2 rounded"></div>
                     <div className="bg-my-blue h-1 m-2 rounded"></div>
@@ -30,25 +33,16 @@ function NavigationBar() {
                 </div>
 
             </div>
-            {
-                onClicked ?
-                    <>
-                        <div className="
-                        drop-shadow-lg
-                        shadow-my-voilet
-                        bg-my-blue-2 fixed left-0 bottom-10 w-fit h-4/5 rounded-r-xl flex flex-col justify-evenly pl-5 pr-5">
+            <div className={`z-40 bg-my-blue-2 fixed left-0 transform transition duration-300 ${isPanelOpen ? 'translate-x-0' : '-translate-x-full'} bottom-10 h-4/5 rounded-r-xl flex flex-col justify-evenly pl-5 pr-5 w-fit`}>
+                <a className="text-my-voilet text-2xl font-semibold font-mono" href="#aboutMe">About Me</a>
+                <a className="text-my-voilet text-2xl font-semibold font-mono" href="#certificates">Certificates</a>
+                <a className="text-my-voilet text-2xl font-semibold font-mono" href="#professionalCertificates">Work</a>
+                <a className="text-my-voilet text-2xl font-semibold font-mono" href="#aptitudeTests">Aptitude</a>
+                <a className="text-my-voilet text-2xl font-semibold font-mono" href="#education">Education</a>
+                <a className="text-my-voilet text-2xl font-semibold font-mono" href="#googleCertificates">Google</a>
+                <a className="text-my-voilet text-2xl font-semibold font-mono" href="#workshopCertificates">Workshop</a>
 
-                            <a className=" text-my-voilet text-2xl font-semibold font-mono" href="#aboutMe">About Me</a>
-                            <a className=" text-my-voilet text-2xl font-semibold font-mono" href="#certificates">Certificates</a>
-                            <a className=" text-my-voilet text-2xl font-semibold font-mono" href="#professionalCertificates">Work</a>
-                            <a className=" text-my-voilet text-2xl font-semibold font-mono" href="#aptitudeTests">Aptitude</a>
-                            <a className=" text-my-voilet text-2xl font-semibold font-mono" href="#education">Education</a>
-                            <a className=" text-my-voilet text-2xl font-semibold font-mono" href="#googleCertificates">Google</a>
-                            <a className=" text-my-voilet text-2xl font-semibold font-mono" href="#workshopCertificates">Workshop</a>
-
-                        </div></> : <>
-                    </>
-            }
+            </div>
         </div>
     );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,17 @@
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      colors:{
+      colors: {
         'my-blue': '#17183B',
         'my-yellow': '#F4E04D',
         'my-blue-2': "#004385",
-        'my-voilet': '#9984D4'
+        'my-voilet': '#9984D4',
+        'my-voilet-2': '#CAA8F5'
       }
     },
   },
   plugins: [],
 }
-
-// --c1: #17183B;
-//   --c2: #004385;
-//   --c3: #F4E04D;
-//   --c4: #CAA8F5;
-//   --c5: #9984D4;

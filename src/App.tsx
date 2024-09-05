47648241fd9aefdbd089e3e3eb66bb9599fc0099
Yash Tibrewal <yashkush.tibrewal@gmail.com>
2024-09-05 14:32:49 +0530

    get in touch

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
 import './css/App.css'
-// import './css/landing-page.css'
-// import Certificates from './components/Certificates';
 import NavigationBar from './components/NavigationBar/NavigationBar';
-// import AptitudeTests from './components/ApptitudeTests'
 import AboutMe from './components/AboutMe';
 import Greet from './components/Greetings';
 import Experience from './components/Experience/Experience';
 import Education from './components/Education/Education';
-// import WorkshopCertificates from './components/Certification/WorkshopCertificates';
-// import GoogleCertifications from './GoogleCertifications';
 import { useEffect, useState } from 'react';
 import eventEmitter from './CustomEventEmitter';
 import Projects from './components/Projects/Projects';
@@ -44,6 +39,7 @@ function App() {
       {/* Gradient background */}
       <div className={`fixed inset-0 bg-gradient-to-b from-my-blue  to-my-blue-2 ${isPanelOpen ? 'blur-lg' : ''}`} />
       <NavigationBar></NavigationBar>
+
       {/* Content */}
       <div className={`relative z-10 bg-transparent text-my-yellow px-4 md:px-8 lg:px-24 xl:px-40 2xl:px-60`}>
         <div onClick={closePanel} className={`pb-20 space-y-10 md:space-y-12 lg:space-y-20 xl:space-y-24 2xl:space-y-32`}>
@@ -53,9 +49,6 @@ function App() {
           <Experience />
           <Projects />
           <Certificates />
-          {/* <AptitudeTests></AptitudeTests> */}
-          {/* <GoogleCertifications></GoogleCertifications> */}
-          {/* <WorkshopCertificates></WorkshopCertificates> */}
         </div>
         <Footer></Footer>
 
